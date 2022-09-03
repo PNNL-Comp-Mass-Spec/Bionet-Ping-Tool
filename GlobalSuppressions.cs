0d4678f859456c6d23dbb5c286a895ee11b442dd
alchemistmatt <matthew.monroe@pnnl.gov>
2022-09-02 17:12:28 -0700

    Suppress warning

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Roslynator", "RCS1233:Use short-circuiting operator.", Justification = "Leave as-is to assure that hostIsActive2 gets initialized", Scope = "member", Target = "~M:BionetPingTool.Program.RemoveInactiveHosts(System.Collections.Generic.ICollection{System.String},System.Collections.Generic.List{System.String}@)")]
