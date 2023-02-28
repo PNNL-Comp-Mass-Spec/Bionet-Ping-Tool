95da3e308d8a7c65aed19ae65d8b0101f5410134
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-28 09:33:13 -0800

    Add comment to Powershell script

--- Export_Script/Export_DNS_Entries.ps1
+++ Export_Script/Export_DNS_Entries.ps1
@@ -1,4 +1,5 @@
 # This script must be run from an admin-level powershell prompt
+# Other WMI Classes: https://docs.microsoft.com/en-us/windows/win32/dns/dns-wmi-classes
 Get-WmiObject `
    -Namespace Root\MicrosoftDNS `
    -Query "SELECT OwnerName, RecordData FROM MicrosoftDNS_AType WHERE ContainerName = 'bionet' AND OwnerName <> 'bionet'" | `
